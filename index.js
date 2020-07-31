9240a63c94fb3b115922081b394db8724de01f1d
Nishu2696 <nishaanth2696@gmail.com>
2020-07-31 11:14:04 +0530

    aws update

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+//scroll-up button which has been used in the UI design is implemented
 const scrollToTop = document.getElementById("scroll-to-top");
 let dataShow = false;
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 50 && !dataShow){
+    if (window.scrollY > 50 && !dataShow) {
         scrollToTop.setAttribute("data-show", "true");
         dataShow = true;
     }
 
-    if ( window.scrollY <= 50 && dataShow){
+    if (window.scrollY <= 50 && dataShow) {
         scrollToTop.setAttribute("data-show", "false");
         dataShow = false;
     }
